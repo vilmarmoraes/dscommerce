7c42c00563737718eabcc0d9429c4e3ca4ed6bfe
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-12-20 14:24:33 -0300

    Payment

--- src/main/java/com/vssolution/dscommerce/entities/Payment.java
+++ src/main/java/com/vssolution/dscommerce/entities/Payment.java
@@ -4,47 +4,39 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_order")
-public class Order implements Serializable {
+@Table(name = "tb_payment")
+public class Payment implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
-	private OrderStatus status;
+	
+	
+	@OneToOne
+	@MapsId
+	private Order order;
 
-	@ManyToOne
-	@JoinColumn(name = "client_id")
-	private User client;
-
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
-
-	public Order() {
-
+	public Payment() {
 	}
-
-	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+	
+	public Payment(Long id, Instant moment, Order order) {
 		this.id = id;
 		this.moment = moment;
-		this.status = status;
-		this.client = client;
-		this.payment = payment;
+		this.order = order;
 	}
 
 	public Long getId() {
@@ -63,28 +55,12 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
-	public OrderStatus getStatus() {
-		return status;
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setStatus(OrderStatus status) {
-		this.status = status;
-	}
-
-	public User getClient() {
-		return client;
-	}
-
-	public void setClient(User client) {
-		this.client = client;
-	}
-
-	public Payment getPayment() {
-		return payment;
-	}
-
-	public void setPayment(Payment payment) {
-		this.payment = payment;
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
 	@Override
@@ -100,8 +76,8 @@ public class Order implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Order other = (Order) obj;
+		Payment other = (Payment) obj;
 		return Objects.equals(id, other.id);
-	}
-
+	}	
+	
 }
