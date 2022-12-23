b07ece184955a50c0c5431803d9d8c4c90b24267
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-12-23 11:36:38 -0300

    POST new product

--- src/main/java/com/vssolution/dscommerce/controllers/ProductController.java
+++ src/main/java/com/vssolution/dscommerce/controllers/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,14 @@ public class ProductController {
 		Page<ProductDTO>  listDto  = service.findAll(pageable);
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	
+	@PostMapping
+	public ProductDTO insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		return dto;
+	}
+	
 	
 		
 }
