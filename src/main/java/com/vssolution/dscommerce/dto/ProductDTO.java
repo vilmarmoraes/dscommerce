3470b302478b1638ca73919756e673d098662ad3
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-12-26 10:40:59 -0300

    Exception handling

--- src/main/java/com/vssolution/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/vssolution/dscommerce/dto/ProductDTO.java
@@ -13,9 +13,6 @@ public class ProductDTO implements Serializable {
 	private Double price;
 	private String imgUrl;
 	
-	public ProductDTO() {
-		
-	}
 
 	public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
@@ -34,46 +31,25 @@ public class ProductDTO implements Serializable {
 		price = entity.getPrice();
 		imgUrl = entity.getImgUrl();
 	}
-	
+
 	public Long getId() {
 		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getDescription() {
 		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
 	}
 
 	public Double getPrice() {
 		return price;
 	}
 
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
 	public String getImgUrl() {
 		return imgUrl;
 	}
-
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
-	}
-	
-	
+		
 }
--- src/main/java/com/vssolution/dscommerce/services/ProductService.java
+++ src/main/java/com/vssolution/dscommerce/services/ProductService.java
@@ -1,64 +1,83 @@
 package com.vssolution.dscommerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vssolution.dscommerce.dto.ProductDTO;
 import com.vssolution.dscommerce.entities.Product;
 import com.vssolution.dscommerce.repositories.ProductRepository;
+import com.vssolution.dscommerce.services.exceptions.DatabaseException;
+import com.vssolution.dscommerce.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-	
-	
+
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
 //		Optional<Product> result = repository.findById(id);
 //		Product product = result.get();
 //		ProductDTO dto = new ProductDTO(product);
 //		return dto;
-		
-		Product product = repository.findById(id).get();
+
+		Product product = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
 		return new ProductDTO(product);
 	}
-	
+
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAll(Pageable pageable) {	
-		Page<Product> result = repository.findAll(pageable);		
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = repository.findAll(pageable);
 		return result.map(x -> new ProductDTO(x));
 	}
 
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
-		Product entity = new Product();		
-		copyDtoToEntiry(dto, entity);		
-		entity = repository.save(entity);		
+		Product entity = new Product();
+		copyDtoToEntiry(dto, entity);
+		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
 
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
-		Product entity = repository.getReferenceById(id);
-		copyDtoToEntiry(dto, entity);		
-		entity = repository.save(entity);		
-		return new ProductDTO(entity);
+		try {
+			Product entity = repository.getReferenceById(id);
+			copyDtoToEntiry(dto, entity);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
 	}
 
 	private void copyDtoToEntiry(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
-		entity.setImgUrl(dto.getImgUrl());		
+		entity.setImgUrl(dto.getImgUrl());
 	}
-	
+
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);		
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Falha de integridade referencial");
+		}			
 	}
-	
 }
